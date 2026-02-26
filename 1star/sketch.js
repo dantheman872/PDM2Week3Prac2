213179f671097b7a95fef2f9e8f0131637199c90
dantheman872 <95766593+dantheman872@users.noreply.github.com>
2026-02-26 12:38:32 +0000

    Update sketch.js

--- 1star/sketch.js
+++ 1star/sketch.js
@@ -1,3 +1,10 @@
+const LEFT_LOWERCASE = "a"
+const LEFT_UPPERCASE = "A"
+const RIGHT_LOWERCASE = "d"
+const UP_LOWERCASE = "w"
+const DOWN_LOWERCASE = "s"
+
+
 function setup() {
     createCanvas(0, 0);
 }
@@ -7,8 +14,27 @@ function draw() {}
 
 function keyPressed() {
     switch (key) {
-        case "a":
+        case LEFT_UPPERCASE:
             console.log("move left");
             break;
+        case LEFT_LOWERCASE:
+            console.log("move left");
+            break;
+
+        case RIGHT_LOWERCASE:
+            console.log("move right");
+            break;
+
+        case UP_LOWERCASE:  
+            console.log("move up");
+            break;
+
+        case DOWN_LOWERCASE:
+            console.log("move down")
+            break;
+        
+        default:
+            console.log("don`t move!")
+            
     }
 }
